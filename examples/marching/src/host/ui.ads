ec6f9f15d9181e85c5da748d9cee0ca0b7b7d194
Pat Bernardi <bernardi@adacore.com>
2021-03-23 21:08:45 +1100

    Reorganise Marching Cubes and make use of the new device-cuda runtime
    The example now makes use of the new device-cuda runtime. The project file
    has also been split into seperate device and host projects because the host
    code depends on the opengl project which is incompatible with the
    device-cuda runtime, and I don't believe there's an easy way to use scenario
    variables to work around that. Beside, the device code is conceptionally
    different code anyway; ideally we could find a way to to make the device
    project a subproject of the host project.
    
    Source code has also been moved around to give the example a bit more
    structure.
    
    TN: U114-058

---
+++ examples/marching/src/host/ui.ads
@@ -0,0 +1,12 @@
+with Volumes; use Volumes;
+
+package UI is
+
+   procedure Initialize;
+
+   procedure Finalize;
+
+   procedure Draw (Shape : Volume; Running : out Boolean);
+
+
+end UI;
--- examples/marching/wrapper.sh
+++ examples/marching/wrapper.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
-KERNEL_SRC_NAME="marching_cubes.adb"
-
-Execution_Side=Device gprbuild -P../../api/cuda
-Execution_Side=Device gprbuild --config=cuda.cgpr -c -P marching_cubes.gpr $KERNEL_SRC_NAME -v
-Execution_Side=Host gprbuild -Pmarching_cubes -largs $PWD/obj/*.fatbin.o
+Execution_Side=Device gprbuild -P ../../api/cuda_device.gpr -j0
+Execution_Side=Device gprbuild -P marching_device_code.gpr -j0
+Execution_Side=Host gprbuild -P marching_cubes -j0 -largs $PWD/obj/*.fatbin.o
