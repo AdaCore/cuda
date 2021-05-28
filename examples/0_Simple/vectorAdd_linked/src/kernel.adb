670eeda2c53f0a3ec45931edca26e3d3436a561b
Ghjuvan Lacambre <lacambre@adacore.com>
2021-05-28 10:04:15 +0200

    Copy vectorAdd example to vectorAdd_linked
    This will serve as the basic example for separate compilation.

---
+++ examples/0_Simple/vectorAdd_linked/src/kernel.adb
@@ -0,0 +1,22 @@
+with CUDA.Runtime_Api; use CUDA.Runtime_Api; -- Block_Dim, Block_IDx, Thread_IDx
+with Interfaces.C;     use Interfaces.C; -- Operators for Block_Dim, Block_IDx, Thread_IDx
+
+package body Kernel is
+   
+   procedure Vector_Add
+     (A_Addr : System.Address;
+      B_Addr : System.Address;
+      C_Addr : System.Address;
+      Num_Elements : Integer)
+   is
+      A : Float_Array (1..Num_Elements) with Address => A_Addr;
+      B : Float_Array (1..Num_Elements) with Address => B_Addr;
+      C : Float_Array (1..Num_Elements) with Address => C_Addr;
+      I : Integer := Integer (Block_Dim.X * Block_IDx.X + Thread_IDx.X);
+   begin
+      if I < Num_Elements then
+         C (C'First + I) := A (A'First + I) + B (B'First + I);
+      end if;
+   end Vector_Add;
+
+end Kernel;
---
+++ examples/0_Simple/vectorAdd_linked/wrapper.sh
@@ -0,0 +1,44 @@
+#!/bin/sh -e
+
+# PATH of the example to run. Should be e.g. examples/0_Simple/vectorAdd/
+EXAMPLE_DIRECTORY="$PWD"
+CUDA_BIN_DIR="/usr/local/cuda/bin/"
+# Min compute capability. E.g. if you're on sm_75, SM_XX should be 75
+SM_XX="75"
+
+OBJ="$EXAMPLE_DIRECTORY/obj"
+SRC="$EXAMPLE_DIRECTORY/src"
+
+KERNEL_SRC_NAME="kernel.adb"
+KERNEL_SRC_PATH="$SRC/$KERNEL_SRC_NAME"
+KERNEL_PTX_NAME="${KERNEL_SRC_NAME%.*}.s"
+KERNEL_PTX_PATH="$OBJ/$KERNEL_PTX_NAME"
+KERNEL_OBJ_NAME="${KERNEL_SRC_NAME%.*}.o"
+KERNEL_OBJ_PATH="$OBJ/$KERNEL_OBJ_NAME"
+KERNEL_FATBIN_NAME="kernel.fatbin"
+KERNEL_FATBIN_PATH="$OBJ/$KERNEL_FATBIN_NAME"
+KERNEL_FATBIN_OBJ="$OBJ/$KERNEL_FATBIN_NAME.o"
+KERNEL_FATBINASM_NAME="$KERNEL_FATBIN_NAME.s"
+KERNEL_FATBINASM_PATH="$SRC/$KERNEL_FATBINASM_NAME"
+
+mkdir -p "$OBJ"
+
+"llvm-gcc" --RTS="$EXAMPLE_DIRECTORY/../../../runtime/" \
+   -I"$EXAMPLE_DIRECTORY/../../../api/device_static/" \
+   -O2 -S -gnatp -gnatn -mcpu=sm_"$SM_XX" --target=nvptx64 \
+   "$KERNEL_SRC_PATH" -o "$KERNEL_PTX_PATH"
+
+# Create GPU object file
+"$CUDA_BIN_DIR/ptxas" -m64 -g --dont-merge-basicblocks --return-at-end -v --gpu-name sm_"$SM_XX" --output-file "$KERNEL_OBJ_PATH" "$KERNEL_PTX_PATH"
+
+# Embed it all in a fat binary
+"$CUDA_BIN_DIR/fatbinary" -64 --create "$KERNEL_FATBIN_PATH" -g --image=profile=sm_"$SM_XX",file="$KERNEL_OBJ_PATH" --image=profile=compute_"$SM_XX",file="$KERNEL_PTX_PATH"
+
+# Create an object out of the fatbinary. Note: we need to move to the object
+# directory to get ld to produce a predictable name.
+CWD_BACKUP="$(pwd)"
+cd "$(dirname "$KERNEL_FATBIN_PATH")"
+ld -r -b binary "$KERNEL_FATBIN_NAME" -o "$KERNEL_FATBIN_OBJ"
+cd "$CWD_BACKUP"
+
+gprbuild -Pmain -largs kernel.fatbin.o
