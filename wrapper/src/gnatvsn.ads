eb1990081b12054165c821551fa5e36d4641d0f1
Ghjuvan Lacambre <lacambre@adacore.com>
2022-01-20 15:09:03 +0100

    V120-001: make wrapper version pluggable
    Gnatvsn.ads is edited by nightly scripts to get a date and version that
    match GNAT's values.

---
+++ wrapper/src/gnatvsn.ads
@@ -0,0 +1,37 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                         GNAT COMPILER COMPONENTS                         --
+--                                                                          --
+--                              G N A T V S N                               --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--          Copyright (C) 1992-2022, Free Software Foundation, Inc.         --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License  distributed with GNAT; see file COPYING3.  If not, go to --
+-- http://www.gnu.org/licenses for a complete copy of the license.          --
+--                                                                          --
+-- GNAT was originally developed  by the GNAT team at  New York University. --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  This package spec holds version information for the GNAT tools.
+--  It is updated whenever the release number is changed.
+
+package Gnatvsn is
+
+   Gnat_Static_Version_String : constant String := "23.0w (19940713)";
+
+   Library_Version : constant String := "23";
+
+   Current_Year : constant String := "2022";
+
+end Gnatvsn;
