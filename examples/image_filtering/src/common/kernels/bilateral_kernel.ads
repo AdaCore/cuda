d7157ec6c8aaf6b4d22db2b2e59f7b2fe5bf66ef
ohenley <olivier.henley@gmail.com>
2022-04-01 18:09:42 -0400

    fix formatting error

--- examples/image_filtering/src/common/kernels/bilateral_kernel.ads
+++ examples/image_filtering/src/common/kernels/bilateral_kernel.ads
@@ -25,13 +25,11 @@ package Bilateral_Kernel is
                         I                 : Integer; 
                         J                 : Integer);
 
-   procedure Bilateral_Cuda (Img_Addr          : System.Address; 
-                             Filtered_Img_Addr : System.Address;
-                             Width             : Integer; 
-                             Height            : Integer; 
-                             Spatial_Stdev     : Float;
-                             Color_Dist_Stdev  : Float; 
-                             I                 : Integer; 
-                             J                 : Integer);
+   procedure Bilateral_Cuda (Device_Img          : System.Address; 
+                             Device_Filtered_Img : System.Address;
+                             Width               : Integer; 
+                             Height              : Integer; 
+                             Spatial_Stdev       : Float;
+                             Color_Dist_Stdev    : Float);
 
 end Bilateral_Kernel;
