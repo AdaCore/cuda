8e51ababc15e9b0cfd6349d311b346f83f0d6472
ohenley <olivier.henley@gmail.com>
2022-06-30 10:43:57 -0400

    changes to make it work

--- examples/image_filtering/src/host/exporter/exporter.adb
+++ examples/image_filtering/src/host/exporter/exporter.adb
@@ -12,29 +12,82 @@
 -- of the license.                                                          --
 ------------------------------------------------------------------------------
 
-with Ada.Strings;       use Ada.Strings;
-with Ada.Strings.Fixed; use Ada.Strings.Fixed;
-with Ada.Text_IO;       use Ada.Text_IO;
+with QOI;
+with System.Storage_Elements;
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with GNAT.OS_Lib;
 
 package body Exporter is
 
-   procedure Export_Image (File_Path : String; Img : G.Image) is
-      File   : File_Type;
-      Width  : constant Natural := Img'Length (1);
-      Height : constant Natural := Img'Length (2);
-   begin
-      Create (File, Out_File, File_Path);
-      Put_Line (File, "P3");
-      Put_Line (File, "# Made using filter_img");
-      Put_Line (File, Trim (Width'Image, Left) & " " & Trim (Height'Image, Left));
-      Put_Line (File, "255");
-      for J in Img'Range (2) loop
-         for I in Img'Range (1) loop
-            Put_Line (File, Trim (Integer (Img (I, J).R)'Image, Left) & " " &
-                            Trim (Integer (Img (I, J).G)'Image, Left) & " " &
-                            Trim (Integer (Img (I, J).B)'Image, Left));
-         end loop;
-      end loop;
-   end;
+   package sse renames System.Storage_Elements;
 
+   procedure Write_To_File (Filename : String;
+                            D : sse.Storage_Array;
+                            Size : sse.Storage_Count)
+   is
+      use GNAT.OS_Lib;
+
+      FD : File_Descriptor;
+      Ret : Integer;
+   begin
+
+      FD := GNAT.OS_Lib.Create_File (Filename, Binary);
+
+      if FD = Invalid_FD then
+         Ada.Text_IO.Put_Line (GNAT.OS_Lib.Errno_Message);
+         GNAT.OS_Lib.OS_Exit (1);
+      end if;
+
+      Ret := Write (FD, D'Address, Integer (Size));
+
+      if Ret /= Integer (Size) then
+         Ada.Text_IO.Put_Line (GNAT.OS_Lib.Errno_Message);
+         GNAT.OS_Lib.OS_Exit (1);
+      end if;
+
+      Close (FD);
+   end Write_To_File;
+
+   procedure Dump_QOI (abs_filename : String; Img : G.Image_Access) is
+   use sse;
+      Desc : QOI.QOI_Desc := (Img'Length(1), Img'Length(2), 3, QOI.SRGB);
+      
+      type Storage_Array_Access is access all sse.Storage_Array;
+
+      function to_storage return Storage_Array_Access is
+         data  : Storage_Array_Access := 
+            new sse.Storage_Array (1 .. Desc.Width * Desc.Height * Desc.Channels);
+         j_offset : Natural;
+         i_offset : Natural;
+         t_offset : Natural;
+      begin
+         for j in img'Range (2) loop
+            j_offset := (j-1) * Natural(Desc.Width * Desc.Channels);
+            for i in img'Range (1) loop
+               i_offset := (i-1) * Natural(Desc.Channels);
+               t_offset := j_offset + i_offset;
+               data (sse.Storage_Offset(t_offset + 1)) := sse.Storage_Element(Img(i,j).R);
+               data (sse.Storage_Offset(t_offset + 2)) := sse.Storage_Element(Img(i,j).G);
+               data (sse.Storage_Offset(t_offset + 3)) := sse.Storage_Element(Img(i,j).B);
+            end loop;
+         end loop;
+         return data;
+      end;
+
+      Data        : Storage_Array_Access := to_storage;
+      Output      : Storage_Array_Access := new sse.Storage_Array (1 .. QOI.Encode_Worst_Case (Desc));
+      Output_Size : sse.Storage_Count;
+   begin
+      QOI.Encode (Data.all, Desc, Output.all, Output_Size);
+
+      if Output_Size /= 0 then
+         Put_Line ("DUMP_QOI : " & abs_filename);
+         Write_To_File (abs_filename, Output.all, Output_Size);
+      else
+         Put_Line ("DUMP_QOI : Encode failed");
+         GNAT.OS_Lib.OS_Exit (1);
+      end if;
+   end;
 end Exporter;
