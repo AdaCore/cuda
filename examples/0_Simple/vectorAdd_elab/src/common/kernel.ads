6ee85a1ceb2ded8795b948338b1cd9cea42da170
Quentin Ochem <ochem@adacore.com>
2022-10-14 07:02:13 -0400

    updated elaboration example following latest fixes

--- examples/0_Simple/vectorAdd_elab/src/common/kernel.ads
+++ examples/0_Simple/vectorAdd_elab/src/common/kernel.ads
@@ -2,6 +2,8 @@ with System;
 
 package Kernel is
 
+   Elaborated_Value : Float := 0.0;
+
    type Float_Array is array (Integer range <>) of Float;
 
    type Access_Host_Float_Array is access all Float_Array;
