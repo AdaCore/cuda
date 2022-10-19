e7e22a5fa11f307ff55c8df1c6348f13a875303a
Quentin Ochem <ochem@adacore.com>
2022-10-19 18:40:34 -0400

    fixed storage models device spec

--- api/device_static/cuda-storage_models.ads
+++ api/device_static/cuda-storage_models.ads
@@ -1,8 +1,6 @@
 with System; use System;
 with System.Storage_Elements; use System.Storage_Elements;
 
-with CUDA.Driver_Types; use CUDA.Driver_Types;
-
 package CUDA.Storage_Models is
 
    type CUDA_Address is new System.Address;
